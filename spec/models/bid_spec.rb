ce091efb3266c29c660cc1c0200f623c771731cd
King <kingsleywang2013@gmail.com>
2019-04-05 12:15:22 +1100

    Added specs for find_match method

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -13,14 +13,111 @@ RSpec.describe Bid, type: :model do
     expect(subject).to be_valid
   end
 
-  describe "find_match" do
+  describe '#find_match' do
+    context 'must match grade and season' do
+      it 'not match both conditions' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test1'
+        bid = Bid.create(season: 2018, grade: 'Test2', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).not_to eq(bid)
+      end
 
-    it 'matches with the highest bid' do
-      offer = Offer.new
-      offer.price = 455
-      offer.quantity = 12
-      bid = Bid.create(price: 455, quantity: 12)
-      expect(Bid.find_match(offer)).to eq(bid)
+      it 'not match grade' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test1'
+        bid = Bid.create(season: 2019, grade: 'Test2', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).not_to eq(bid)
+      end
+
+      it 'not match season' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2018, grade: 'Test', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).not_to eq(bid)
+      end
+
+      it 'match both conditions' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).to eq(bid)
+      end
+    end
+
+    context 'quantity must greater than offers' do
+      it 'quantity less than offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 455, quantity: 10)
+        expect(Bid.find_match(offer)).not_to eq(bid)
+      end
+
+      it 'quantity equal than offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).to eq(bid)
+      end
+
+      it 'quantity greater than offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 455, quantity: 20)
+        expect(Bid.find_match(offer)).to eq(bid)
+      end
+    end
+
+    context 'price must greater than offers' do
+      it 'price less than offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 400, quantity: 12)
+        expect(Bid.find_match(offer)).not_to eq(bid)
+      end
+
+      it 'price equal to offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 455, quantity: 12)
+        expect(Bid.find_match(offer)).to eq(bid)
+      end
+
+      it 'price greater than offers' do
+        offer = Offer.new
+        offer.price = 455
+        offer.quantity = 12
+        offer.season = 2019
+        offer.grade = 'Test'
+        bid = Bid.create(season: 2019, grade: 'Test', price: 500, quantity: 12)
+        expect(Bid.find_match(offer)).to eq(bid)
+      end
     end
 
     it 'matches with the highest bid' do
@@ -30,11 +127,29 @@ RSpec.describe Bid, type: :model do
 
       # bids
       highest_bid = Bid.create(price: 485, quantity: 50)
-      Bid.create(price: 475, quantity: 50)
-      Bid.create(price: 465, quantity: 50)
+      second_bid = Bid.create(price: 475, quantity: 50)
+      last_bid = Bid.create(price: 465, quantity: 50)
 
       expect(Bid.find_match(offer)).to eq(highest_bid)
+      expect(Bid.find_match(offer)).not_to eq(second_bid)
+      expect(Bid.find_match(offer)).not_to eq(last_bid)
     end
 
+    it 'choose first create bid if max prices are the same' do
+      offer = Offer.new
+      offer.price = 455
+      offer.quantity = 12
+      offer.season = 2019
+      offer.grade = 'Test'
+
+      # bids
+      first_create_highest_bid = Bid.create(season: 2019, grade: 'Test', price: 485, quantity: 50)
+      second_create_highest_bid = Bid.create(season: 2019, grade: 'Test', price: 485, quantity: 50)
+      last_bid = Bid.create(season: 2019, grade: 'Test', price: 465, quantity: 50)
+
+      expect(Bid.find_match(offer)).to eq(first_create_highest_bid)
+      expect(Bid.find_match(offer)).not_to eq(second_create_highest_bid)
+      expect(Bid.find_match(offer)).not_to eq(last_bid)
+    end
   end
 end
