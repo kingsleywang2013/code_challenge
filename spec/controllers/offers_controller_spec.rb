73c1e92dba38ff7b3d4e23e72eb44ac3aaab9d84
King <kingsleywang2013@gmail.com>
2019-04-05 11:24:09 +1100

    Added specs for offer destroy

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe OffersController, type: :controller do
   it '#index as root path' do
     expect(get: root_url(subdomain: nil)).to route_to(controller: 'offers', action: 'index')
   end
+
+  context '#destroy' do
+    before :each do
+      @offer = Offer.create(season: 2019, price: 200, quantity: 20, grade: 'Test')
+      delete :destroy, params: { id: @offer.id }
+    end
+
+    it 'destroys role' do
+      expect(assigns(@offer).destroyed?).to be true
+    end
+  end
 end
