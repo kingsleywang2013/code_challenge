7daec75a5ab6d6378fa74bbb747d15ca1e7abf0a
King <kingsleywang2013@gmail.com>
2019-04-05 12:39:06 +1100

    Added specs for trade to get price from bid and get quantity from offer

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Trade, type: :model do
     expect(trade.offer).to eq offer
     expect(trade.bid).to eq bid
   end
+
+  it 'get price from bid' do
+    bid = Bid.create(season: 2019, grade: 'Test', price: 500, quantity: 25)
+    offer = Offer.create(season: 2019, grade: 'Test', price: 455, quantity: 20)
+    trade = Trade.create(bid: bid, offer: offer)
+    expect(trade.price).to eq bid.price
+  end
+
+  it 'get quantity from offer' do
+    bid = Bid.create(season: 2019, grade: 'Test', price: 500, quantity: 25)
+    offer = Offer.create(season: 2019, grade: 'Test', price: 455, quantity: 20)
+    trade = Trade.create(bid: bid, offer: offer)
+    expect(trade.quantity).to eq offer.quantity
+  end
 end
