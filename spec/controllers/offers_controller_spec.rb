13a99a9b40a646cc52057eebacb578e65c484e50
King <kingsleywang2013@gmail.com>
2019-04-05 11:41:39 +1100

    Added specs for offer update

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe OffersController, type: :controller do
       expect(Offer.all.size).to be 0
     end
   end
+
+  context '#update' do
+    before :each do
+      @offer = Offer.create(season: 2019, price: 200, quantity: 20, grade: 'Test')
+    end
+
+    it 'update with invalid data' do
+      patch :update, params: { id: @offer.id, season: 2019.1}
+      expect(@offer.errors.full_messages.size).not_to be 0
+    end
+
+    it 'update with valid data' do
+      patch :update, params: { id: @offer.id, season: 2018}
+      expect(@offer.errors.full_messages.size).to be 0
+    end
+  end
 end
