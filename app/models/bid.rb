d157a91c7d4890ba892e5d176f0e2686b33d6978
King <kingsleywang2013@gmail.com>
2019-04-05 12:28:30 +1100

    Implementd find_match method

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,9 +2,17 @@ class Bid < ApplicationRecord
   validates :quantity, numericality: { greater_than: 0 }
   validates :price, numericality: { greater_than: 0 }
 
+  scope :match_season_grade, ->(offer) { where(season: offer.season).where(grade: offer.grade) }
+  scope :match_quantity_rule, ->(offer) { where('quantity >= ?', offer.quantity) }
+  scope :match_price_rule, ->(offer) { where('price >= ?', offer.price) }
+  scope :order_by_max_price_first_create, -> { order(price: :desc).order(created_at: :asc) }
+
   class << self
     def find_match(offer)
-      Bid.where(price: offer.price).first
+      Bid.match_season_grade(offer)
+          .match_quantity_rule(offer)
+          .match_price_rule(offer)
+          .order_by_max_price_first_create.first
     end
   end
 end
