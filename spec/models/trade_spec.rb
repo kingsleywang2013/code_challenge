b2c3ecafb237ec39a48aa969f093a8dc347e0487
King <kingsleywang2013@gmail.com>
2019-04-05 12:32:06 +1100

    Added specs for trade as reference to offer and bid

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'reference to offer and bid' do
+    bid = Bid.create(season: 2019, grade: 'Test', price: 500, quantity: 25)
+    offer = Offer.create(season: 2019, grade: 'Test', price: 455, quantity: 20)
+    trade = Trade.create(bid: bid, offer: offer)
+    expect(trade.offer).to include offer
+    epxect(trade.bid).to include bid
+  end
 end
