23960ac771e14aee878b61125ea731bda45e8625
King <kingsleywang2013@gmail.com>
2019-04-05 11:08:48 +1100

    Added spec for checking offer's quantity and price

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Offer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'price should greater than 0' do
+    offer = Offer.create(price: -10)
+    expect(offer).not_to be_valid
+  end
+
+  it 'quantity should greater than 0' do
+    offer = Offer.create(quantity: -10)
+    expect(offer).not_to be_valid
+  end
+
+  it 'price should less than 10_000' do
+    offer = Offer.create(price: 10_001)
+    expect(offer).not_to be_valid
+  end
+
+  it 'quantity should greater than 10_000' do
+    offer = Offer.create(quantity: 10_001)
+    expect(offer).not_to be_valid
+  end
 end
