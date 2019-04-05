85591ab0d4522bb194ec3c075b18e23e4b0c35c7
King <kingsleywang2013@gmail.com>
2019-04-05 11:10:50 +1100

    Updated new validation for offer's quantity and price

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,9 @@
 class Offer < ApplicationRecord
-  validates :quantity, numericality: { greater_than: 0 }
-  validates :price, numericality: { greater_than: 0 }
+  module Constants
+    MAX_QUANTITY = 10_000
+    MAX_PRICE = 10_000
+  end
+
+  validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: Constants::MAX_QUANTITY }
+  validates :price, numericality: { greater_than: 0, less_than_or_equal_to: Constants::MAX_PRICE }
 end
